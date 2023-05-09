f45a662b60a64d01edc1d4a8639801368a336f3f
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-09 17:40:26 +0600

    loaded data from the database and added delete button

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddUser = () => {
+
+    // want to keep many fields in a one state should use this system
+    const [user, setUser] = useState({});
+
+    const handleAddUser = e => {
+        e.preventDefault();
+        console.log(user);
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    alert('User added successfully');
+                }
+            })
+
+        e.target.reset();
+    };
+
+    const handleInputBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+
+        const newUser = { ...user }; // being state is used optional chaining 
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
     return (
         <div>
             <h2> Please add a user</h2>
+
+            <form onSubmit={handleAddUser}>
+                <input onBlur={handleInputBlur} type="text" name='name' placeholder='name' required />
+                <br />
+                <input onBlur={handleInputBlur} type="text" name='address' placeholder='address' required />
+                <br />
+                <input onBlur={handleInputBlur} type="email" name="email" id="" placeholder='email' required />
+                <br />
+                <button type="submit">Add User</button>
+            </form>
         </div>
     );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+
+    const users = useLoaderData()
+
+    const handleDelete = user => {
+        const agree = window.confirm(`Are you sure? You want to delete: ${user.name}`);
+        if (agree) {
+            console.log(`Deleting user with id: ${user._id}`);
+        }
+    };
+
     return (
         <div>
-            <h2>Home</h2>
+            <h2>Users: {users.length}</h2>
+
+            <div>
+                {
+                    users.map(user => <p key={user._id}>
+                        {user.name} : {user.email} <button onClick={() => handleDelete(user)}>X</button>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
