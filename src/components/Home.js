3595592d916b8bacb82000c2aa406b8f0936efc7
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-09 19:53:12 +0600

    learnt how to delete something from the database and added some comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
 
-    const users = useLoaderData()
+    const users = useLoaderData();
+    // this state is declared for removing data form the client side without refresh
+    const [displayUsers, setDisplayUsers] = useState(users);
 
     const handleDelete = user => {
+        // to take opinion of the user
         const agree = window.confirm(`Are you sure? You want to delete: ${user.name}`);
         if (agree) {
-            console.log(`Deleting user with id: ${user._id}`);
+            // console.log(`Deleting user with id: ${user._id}`);
+            // to send data to the database which user will be deleted from the database
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // to inform about deleting confirmation to the user
+                    if (data.deletedCount > 0) {
+                        alert('Deleted user successfully.');
+                        // to get remainig users 
+                        const remainingUsers = displayUsers.filter(usr => usr._id !== user._id);
+                        setDisplayUsers(remainingUsers);
+                    }
+                })
         }
     };
 
     return (
         <div>
-            <h2>Users: {users.length}</h2>
+            <h2>Users: {displayUsers.length}</h2>
 
             <div>
                 {
-                    users.map(user => <p key={user._id}>
+                    displayUsers.map(user => <p key={user._id}>
                         {user.name} : {user.email} <button onClick={() => handleDelete(user)}>X</button>
                     </p>)
                 }
